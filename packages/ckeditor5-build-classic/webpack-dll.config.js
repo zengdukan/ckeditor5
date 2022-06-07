6482738187041ff3eef346b8be49810d010c5f93
Paweł Smyrek <p.smyrek@cksource.com>
2022-06-07 13:04:49 +0200

    Added support for bundling all translations from all packages

--- packages/ckeditor5-build-classic/webpack-dll.config.js
+++ packages/ckeditor5-build-classic/webpack-dll.config.js
@@ -8,19 +8,17 @@
 /* eslint-env node */
 
 const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const glob = require( 'glob' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 
-module.exports = {
+const editorConfig = {
 	devtool: 'source-map',
+
 	performance: { hints: false },
 
 	entry: path.resolve( __dirname, 'src', 'ckeditor-dll.js' ),
 
 	output: {
-		// The name under which the editor will be exported.
 		library: [ 'CKEditor5', 'builds', 'ClassicEditor' ],
 		path: path.resolve( __dirname, 'build' ),
 		filename: 'ckeditor-dll.js',
@@ -41,54 +39,46 @@ module.exports = {
 				extractComments: false
 			} )
 		]
-	},
-
-	plugins: [
-		new CKEditorWebpackPlugin( {
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: 'en',
-			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
-	],
-
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				use: [ 'raw-loader' ]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: 'style-loader',
-						options: {
-							injectType: 'singletonStyleTag',
-							attributes: {
-								'data-cke': true
-							}
-						}
-					},
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: styles.getPostCssConfig( {
-								themeImporter: {
-									themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-								},
-								minify: true
-							} )
-						}
-					}
-				]
-			}
-		]
 	}
 };
+
+const translationsConfig = {
+	performance: { hints: false },
+
+	entry: groupAllTranslations(),
+
+	output: {
+		path: path.resolve( __dirname, 'build', 'translations-dll' ),
+		filename: '[name].js'
+	}
+};
+
+function groupAllTranslations() {
+	const names = [
+		'ckeditor5',
+		'ckeditor5-collaboration',
+		'@ckeditor/ckeditor5-*'
+	];
+
+	const globPattern = `node_modules/{${ names.join( ',' ) }}/build/translations/*.js`;
+
+	const globConfig = {
+		absolute: true,
+		cwd: path.resolve( __dirname, '..', '..' ),
+		ignore: 'ckeditor5-build-*'
+	};
+
+	return glob
+		.sync( globPattern, globConfig )
+		.reduce( ( result, foundPath ) => {
+			const language = path.basename( foundPath, '.js' );
+
+			result[ language ] = result[ language ] || [];
+			result[ language ].push( foundPath );
+
+			return result;
+		}, {} );
+}
+
+module.exports = [ editorConfig, translationsConfig ];
 
